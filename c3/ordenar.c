f36f1826f81d58586d4ae20e4c99d983233f7f16
unknown <zintiamilagrosmeza@gmail.com>
2022-12-20 09:42:16 -0500

    funcion burbuja implementaria

--- c3/ordenar.c
+++ c3/ordenar.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
-
+void burbuja(int arreglo[],int longitud);
+//void intercambiar(int*a, int*b);
 int main(void)
 {
     int num_ordenar[] = {30,25,12,34,-4,-53,56,200,43,28};
 
-    //mostrando datos del arreglo no ordenado
+    //obtener datos del arreglo
 
     int longitud_arreglo_bytes = sizeof(num_ordenar);
-    int longitud_elemento = sizeof(num_ordenar[0])
+    int longitud_elemento = sizeof(num_ordenar[0]);
     int longitud_arreglo = longitud_arreglo_bytes/longitud_elemento;
 
-    print("Datos del arreglo sin ordenar : ");
+    printf("Datos del arreglo sin ordenar : ");
     for(int i = 0; i < longitud_arreglo; i++)
     {
-        print("%d ",num_ordenar[i]);
+        printf("%d ",num_ordenar[i]);
     }
-    print("\n");
+    printf("\n");
 
+    burbuja(num_ordenar, longitud_arreglo);
+}
+
+void burbuja(int arreglo[],int longitud)
+{
+    for(int j = 0; j < longitud; j++)
+    {
+        for(int k = 0; k < longitud-j-1; k++)
+        {
+           if(arreglo[k] > arreglo[k+1])
+           {
+            //intercambiar(&arreglo[k],&arreglo[k+1]);
+           }
+        }
+    }
 }
